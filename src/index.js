ca5dcf106ebcd8985e7df4b6d4d72a3f3d3e3e0c
DIonysos Dajka <diondiondion@gmail.com>
2018-09-04 17:27:12 +0200

    Add babel transform & npm publishing setup

---
+++ src/index.js
@@ -0,0 +1,11 @@
+export {default as Base} from './Base';
+export {default as Button} from './Button';
+export {default as ButtonCore} from './ButtonCore';
+export {default as Icon} from './Icon';
+export {default as InlineList} from './InlineList';
+export {default as TextLink} from './TextLink';
+export {default as ThemeSection} from './ThemeSection';
+
+export {default as mixins} from './mixins';
+export {default as theme} from './theme';
+export {default as utils} from './utils';
