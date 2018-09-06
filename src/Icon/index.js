927588a87fe2424877aae513c29d379fe3122374
DIonysos Dajka <diondiondion@gmail.com>
2018-09-06 18:53:10 +0200

    New icon wrapper component, renders 1 element less per icon

--- src/Icon/index.js
+++ src/Icon/index.js
@@ -1,75 +1,28 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import styled, {css} from 'styled-components';
 
 import {getSpacing} from '../utils/spacing';
-import {pxToRem, pxToEm} from '../utils/units';
 
 import iconMap from './iconMap';
 
-const iconSize = 18;
-
 const sizeMap = {
-	unscaled: pxToRem(iconSize),
-	default: pxToEm(iconSize),
-	medium: pxToEm(iconSize * 1.5),
-	big: pxToEm(iconSize * 3),
-	huge: pxToEm(iconSize * 6),
+	unscaled: 0,
+	default: 1,
+	medium: 1.5,
+	big: 3,
+	huge: 6,
 };
 
-const Wrapper = styled.span`
-	display: inline-block;
-	vertical-align: middle;
-
-	flex: 0 0 auto;
-	/* Using ems makes the icon scale along with the surrounding font size */
-	width: 1em;
-	height: 1em;
-
-	${p => p.spacingLeft && css`
-		margin-left: ${getSpacing(p.spacingLeft)};
-	`}
-	${p => p.spacingRight && css`
-		margin-right: ${getSpacing(p.spacingRight)};
-	`}
-
-	font-size: ${p => sizeMap[p.size || 'default']};
-
-	${p => p.vAlign && css`
-		/* Use to align icons with surrounding body text */
-		position: relative;
-		top: -0.12em;
-	`}
-
-	& > svg {
-		vertical-align: top;
-		fill: currentColor;
-	}
-`;
-
-const Icon = ({name, size, vAlign, spacingLeft, spacingRight, ...otherProps}) => {
+const Icon = ({name, scale, size, spacingLeft, spacingRight, ...otherProps}) => {
 	const Glyph = iconMap[name] || iconMap.x;
 
 	return (
-		<Wrapper
-			size={size}
-			vAlign={vAlign}
-			spacingLeft={spacingLeft}
-			spacingRight={spacingRight}
-			role="presentation"
+		<Glyph
+			scale={scale || sizeMap[size]}
+			spacingLeft={getSpacing(spacingLeft)}
+			spacingRight={getSpacing(spacingRight)}
 			{...otherProps}
-		>
-			<Glyph />
-		</Wrapper>
+		/>
 	);
-};
-
-Icon.propTypes = {
-	name: PropTypes.string.isRequired,
-	size: PropTypes.string,
-	vAlign: PropTypes.bool,
-	spacingLeft: PropTypes.string,
-	spacingRight: PropTypes.string,
 };
 
 export default Icon;
