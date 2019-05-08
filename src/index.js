c607b75b08cfa0731b028459ad462c3fd8eac383
DIonysos Dajka <diondiondion@gmail.com>
2019-05-08 16:02:37 +0100

    Fix default exports for new components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
+export {default as styleProps} from './styleProps';
+export {default as icons} from './icons';
 export {default as mixins} from './mixins';
 export {default as theme} from './theme';
 export {default as utils} from './utils';
-export {default as icons} from './icons';
 
-export {default as Base} from './Base';
+export {default as Box} from './Box';
 export {default as Button} from './Button';
 export {default as ButtonCore} from './ButtonCore';
+export {default as Flex} from './Flex';
 export {default as Icon} from './Icon';
 export {default as InlineList} from './InlineList';
+export {default as PopOver} from './PopOver';
+export {default as Portal} from './Portal';
+export {default as Switch} from './Switch';
+export {default as Text} from './Text';
 export {default as TextLink} from './TextLink';
 export {default as ThemeSection} from './ThemeSection';
-export {default as Switch} from './Switch';
