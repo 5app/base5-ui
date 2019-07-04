d9bc0707062b908fb62da2108e7bee3ab32ff589
Andy <andy.mpanda@gmail.com>
2019-07-04 16:08:17 +0100

    test: ButtonCore

--- src/ButtonCore/index.spec.js
+++ src/ButtonCore/index.spec.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {render, cleanup} from '@testing-library/react';
-// import Wrapper from '../../test/helper/wrapper';
+import Wrapper from '../../test/helper/wrapper';
 import {ThemeSectionError} from '../ThemeSection';
 import ButtonCore from '.';
+import RouterLink from './routerLinkForReadme';
+import 'jest-dom/extend-expect';
 
 describe('ButtonCore', () => {
 	afterEach(cleanup);
@@ -11,5 +13,37 @@ describe('ButtonCore', () => {
 		expect(() => {
 			render(<ButtonCore />).toThrow(ThemeSectionError);
 		});
+	});
+
+	it('renders as a button by default', () => {
+		const {container} = render(
+			<Wrapper>
+				<ButtonCore id="ok">Ok</ButtonCore>
+			</Wrapper>
+		);
+
+		const ok = container.querySelector('#ok');
+		expect(ok).toBeInTheDocument();
+		expect(ok.tagName).toBe('BUTTON');
+	});
+
+	it('can render as a RouterLink', () => {
+		const path = 'path/to/whatever';
+		const {container} = render(
+			<Wrapper>
+				<ButtonCore id="ok" as={RouterLink} to={path}>
+					Ok
+				</ButtonCore>
+			</Wrapper>
+		);
+
+		const ok = container.querySelector('#ok');
+		expect(ok).toBeInTheDocument();
+		expect(ok.tagName).toBe('A');
+		expect(ok.href).toContain(path);
+	});
+
+	it('can render as RouterLink', () => {
+		expect(true).toBe(true);
 	});
 });
