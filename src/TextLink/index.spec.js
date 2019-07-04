a771017b1f662540abc4e753fde2397deaeacd7c
Andy <andy.mpanda@gmail.com>
2019-07-04 12:25:34 +0100

    test: TextLink renders as link or button

--- src/TextLink/index.spec.js
+++ src/TextLink/index.spec.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {render, cleanup} from '@testing-library/react';
-// import Wrapper from '../../test/helper/wrapper';
+import Wrapper from '../../test/helper/wrapper';
 import {ThemeSectionError} from '../ThemeSection';
 import TextLink from '.';
+import 'jest-dom/extend-expect';
 
 describe('TextLink', () => {
 	afterEach(cleanup);
@@ -11,5 +12,31 @@ describe('TextLink', () => {
 		expect(() => {
 			render(<TextLink />).toThrow(ThemeSectionError);
 		});
+	});
+
+	it('renders as a link by default', () => {
+		const path = '/path/to/whatever';
+		const {getByText} = render(
+			<Wrapper>
+				<TextLink href={path}>Click!</TextLink>
+			</Wrapper>
+		);
+
+		const link = getByText('Click!');
+		expect(link).toBeInTheDocument();
+		expect(link.tagName).toBe('A');
+		expect(link.href).toContain(path);
+	});
+
+	it('can render as a button', () => {
+		const {getByText} = render(
+			<Wrapper>
+				<TextLink as="button">Click!</TextLink>
+			</Wrapper>
+		);
+
+		const link = getByText('Click!');
+		expect(link).toBeInTheDocument();
+		expect(link.tagName).toBe('BUTTON');
 	});
 });
