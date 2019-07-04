a8de0cfa656dffc21bb377a7fff52d0ce2f3c56d
Andy <andy.mpanda@gmail.com>
2019-07-04 17:59:07 +0100

    test: Flex

--- src/Flex/index.spec.js
+++ src/Flex/index.spec.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {render, cleanup} from '@testing-library/react';
-// import Wrapper from '../../test/helper/wrapper';
+import Wrapper from '../../test/helper/wrapper';
 import {ThemeSectionError} from '..//ThemeSection';
 import Flex from '.';
+import 'jest-dom/extend-expect';
 
 describe('Flex', () => {
 	afterEach(cleanup);
@@ -11,5 +12,43 @@ describe('Flex', () => {
 		expect(() => {
 			render(<Flex />).toThrow(ThemeSectionError);
 		});
+	});
+
+	it('can render wrapped content', () => {
+		const {container} = render(
+			<Wrapper>
+				<div>
+					<Flex id="flex">Hello, World!</Flex>
+					<Flex id="wrap" wrap="true">
+						Hello, World!
+					</Flex>
+				</div>
+			</Wrapper>
+		);
+
+		const flex = container.querySelector('#flex');
+		const wrap = container.querySelector('#wrap');
+
+		expect(wrap).toHaveStyle('flex-wrap: wrap');
+		expect(flex).not.toHaveStyle('flex-wrap: wrap');
+	});
+
+	it('can render content as a column', () => {
+		const {container} = render(
+			<Wrapper>
+				<div>
+					<Flex id="flex">Hello, World!</Flex>
+					<Flex id="column" column>
+						Hello, World!
+					</Flex>
+				</div>
+			</Wrapper>
+		);
+
+		const flex = container.querySelector('#flex');
+		const column = container.querySelector('#column');
+
+		expect(column).toHaveStyle('flex-direction: column');
+		expect(flex).not.toHaveStyle('flex-direction: column');
 	});
 });
