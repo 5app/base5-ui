bb06198ae6b916b13b29a53ca7b5972bae1d679b
Andy <andy.mpanda@gmail.com>
2019-07-04 10:15:59 +0100

    test: Text renders formatted text

--- src/Text/index.spec.js
+++ src/Text/index.spec.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {render, cleanup} from '@testing-library/react';
-// import Wrapper from '../../test/helper/wrapper';
+import Wrapper from '../../test/helper/wrapper';
 import {ThemeSectionError} from '../ThemeSection';
 import Text from '.';
+import 'jest-dom/extend-expect';
 
 describe('Text', () => {
 	afterEach(cleanup);
@@ -11,5 +12,20 @@ describe('Text', () => {
 		expect(() => {
 			render(<Text />).toThrow(ThemeSectionError);
 		});
+	});
+
+	it('renders styled text', () => {
+		const {getByText} = render(
+			<Wrapper>
+				<Text as="div" bold>
+					Hello, World!
+				</Text>
+			</Wrapper>
+		);
+
+		const text = getByText('Hello, World!');
+		expect(text).toBeInTheDocument();
+		expect(text).toHaveStyle('font-weight: bold');
+		expect(text.tagName).toBe('DIV');
 	});
 });
