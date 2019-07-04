bd3cb4bef7746e33555862aaf8d682d37425f873
Andy <andy.mpanda@gmail.com>
2019-07-04 14:22:20 +0100

    test: Switch renders with initialized states

--- src/Switch/index.spec.js
+++ src/Switch/index.spec.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {render, cleanup} from '@testing-library/react';
-// import Wrapper from '../../test/helper/wrapper';
+import Wrapper from '../../test/helper/wrapper';
 import {ThemeSectionError} from '../ThemeSection';
 import Switch from '.';
+import 'jest-dom/extend-expect';
 
 describe('Switch', () => {
 	afterEach(cleanup);
@@ -11,5 +12,30 @@ describe('Switch', () => {
 		expect(() => {
 			render(<Switch />).toThrow(ThemeSectionError);
 		});
+	});
+
+	it('renders with initialized states', () => {
+		const {container} = render(
+			<Wrapper>
+				<div>
+					<Switch id="checked" checked readOnly />
+					<Switch id="disabled" disabled readOnly />
+					<Switch id="unchecked" checked={false} readOnly />
+				</div>
+			</Wrapper>
+		); // readOnly to avoid warning message
+
+		const checked = container.querySelector('#checked');
+		const disabled = container.querySelector('#disabled');
+		const unchecked = container.querySelector('#unchecked');
+
+		expect(checked).toBeInTheDocument();
+		expect(disabled).toBeInTheDocument();
+		expect(unchecked).toBeInTheDocument();
+
+		expect(checked.checked).toBe(true);
+		expect(disabled.checked).toBe(false);
+		expect(disabled.disabled).toBe(true);
+		expect(unchecked.checked).toBe(false);
 	});
 });
