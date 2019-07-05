8c6533f0089bff93cddfd791abfa4edc631a9cf6
Andy <andy.mpanda@gmail.com>
2019-07-05 15:44:36 +0100

    test: icons

--- src/Icon/index.spec.js
+++ src/Icon/index.spec.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {render, cleanup} from '@testing-library/react';
-// import Wrapper from '../../test/helper/wrapper';
+import Wrapper from '../../test/helper/wrapper';
 import {ThemeSectionError} from '../ThemeSection';
 import Icon from '.';
+import iconMap from './iconMap';
+import X from '../icons/X';
 
 describe('Icon', () => {
 	afterEach(cleanup);
@@ -10,6 +12,52 @@ describe('Icon', () => {
 	it('throws ThemeSectionError if rendered outside a <ThemeSection />', () => {
 		expect(() => {
 			render(<Icon />).toThrow(ThemeSectionError);
+		});
+	});
+
+	it('loads the X icon for unknown icon names', () => {
+		const {container} = render(
+			<Wrapper>
+				<div>
+					<X id="x" />
+					<Icon id="unknown" name="unknown" />
+				</div>
+			</Wrapper>
+		);
+
+		const x_path = container.querySelector('#x path').getAttribute('d');
+		const unknown_path = container
+			.querySelector('#unknown path')
+			.getAttribute('d');
+
+		expect(unknown_path).toBe(x_path);
+	});
+
+	it('can load any icon from the iconMap', () => {
+		const {container} = render(
+			<Wrapper>
+				<X />
+			</Wrapper>
+		);
+
+		const x_path = container.querySelector('svg path').getAttribute('d');
+
+		Object.keys(iconMap).forEach(name => {
+			const {container} = render(
+				<Wrapper>
+					<Icon id="icon" name={name} />
+				</Wrapper>
+			);
+
+			const icon_path = container
+				.querySelector('svg path')
+				.getAttribute('d');
+
+			if (name === 'x') {
+				expect(icon_path).toBe(x_path);
+			} else {
+				expect(icon_path).not.toBe(x_path);
+			}
 		});
 	});
 });
