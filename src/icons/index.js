d7e1c1c74aa4c611257531483d6e99875b99108e
DIonysos Dajka <diondiondion@gmail.com>
2019-08-15 11:13:16 +0200

    fix: Remove broken iconMap import

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,5 +1,3 @@
-export {default as iconMap} from './iconMap';
-
 export {default as Android} from './Android';
 export {default as Apple} from './Apple';
 export {default as Appstore} from './Appstore';
