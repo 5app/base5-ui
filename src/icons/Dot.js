66b7b7c9fa431a78c612c7c315968bcff9543764
DIonysos Dajka <diondiondion@gmail.com>
2019-08-16 16:42:17 +0200

    feat: Added new icons NavUp, NavDown, Dot

---
+++ src/icons/Dot.js
@@ -0,0 +1,31 @@
+import React, {forwardRef} from 'react';
+
+import Svg from './BaseSvg';
+
+const DotIcon = forwardRef((props, ref) => {
+	const {size, color, ...otherProps} = props;
+
+	return (
+		<Svg
+			{...otherProps}
+			ref={ref}
+			viewBox="0 0 18 18"
+			width={size}
+			height={size}
+			fill={color}
+			fillRule="evenodd"
+			clipRule="evenodd"
+		>
+			<path d="M 9 4 A 5 5 0 0 1 9 14 A 5 5 0 0 1 9 4" />
+		</Svg>
+	);
+});
+
+DotIcon.displayName = 'DotIcon';
+
+DotIcon.defaultProps = {
+	size: 18,
+	color: 'currentcolor',
+};
+
+export default DotIcon;
---
+++ src/icons/NavDown.js
@@ -0,0 +1,31 @@
+import React, {forwardRef} from 'react';
+
+import Svg from './BaseSvg';
+
+const NavDownIcon = forwardRef((props, ref) => {
+	const {size, color, ...otherProps} = props;
+
+	return (
+		<Svg
+			{...otherProps}
+			ref={ref}
+			viewBox="0 0 18 18"
+			width={size}
+			height={size}
+			fill={color}
+			fillRule="evenodd"
+			clipRule="evenodd"
+		>
+			<path d="M14,5l-5,9l-5,-9l10,0Z" />
+		</Svg>
+	);
+});
+
+NavDownIcon.displayName = 'NavDownIcon';
+
+NavDownIcon.defaultProps = {
+	size: 18,
+	color: 'currentcolor',
+};
+
+export default NavDownIcon;
