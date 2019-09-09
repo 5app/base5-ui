718bc258a8b316d3f1d88a07fbdc14a0d1efc28b
DIonysos Dajka <diondiondion@gmail.com>
2019-09-09 17:25:49 +0200

    style: Add basic reset to docz wrapper

--- docz/Wrapper/index.js
+++ docz/Wrapper/index.js
@@ -14,6 +14,13 @@ const GlobalStyle = createGlobalStyle`
 	*:focus:not(.focus-visible) {
 		outline: none;
 	}
+
+	html, body, div, span, h1, h2, h3, h4, h5, p, blockquote, pre, a, dl, dt, dd, ol, ul, li, fieldset, form, label, article, aside, header, footer, menu, nav, section, audio, video, figure, figcaption {
+		margin: 0;
+		padding: 0;
+		border: 0;
+		vertical-align: baseline;
+	}
 `;
 
 const Wrapper = ({children}) => (
