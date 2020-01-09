cac28c5d2ef035238dd6c121b0ec300717eef49a
DIonysos Dajka <diondiondion@gmail.com>
2020-01-09 13:33:15 +0100

    feat: New mergeRefs utility for merging react refs

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,5 @@
 export {default as colors} from './colors';
+export {default as mergeRefs} from './mergeRefs';
 export {default as spacing} from './spacing';
 export {default as truncateText} from './truncateText';
 export {default as units} from './units';
---
+++ src/utils/mergeRefs.js
@@ -0,0 +1,1 @@
+export {default as mergeRefs} from 'react-merge-refs';
