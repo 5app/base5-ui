30adbfcdee284a6a18f4c18724fc906df813562e
DIonysos Dajka <diondiondion@gmail.com>
2020-01-09 13:53:54 +0100

    fix: Fix broken import

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,4 +3,3 @@ export {default as mergeRefs} from './mergeRefs';
 export {default as spacing} from './spacing';
 export {default as truncateText} from './truncateText';
 export {default as units} from './units';
-export {default as theme} from './theme';
