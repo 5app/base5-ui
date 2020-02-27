0978082220e2e0dd28ed92e7a059aad65a9d73b7
DIonysos Dajka <diondiondion@gmail.com>
2020-02-27 16:59:29 +0100

    feat(utils): getLength now converts fractions to %

--- src/utils/getLength.js
+++ src/utils/getLength.js
@@ -1,17 +1,28 @@
-import {pxToRem} from './units';
+import {pxToRem, fractionToPercent} from './units';
 
-function getLength(length) {
-	const lengthAsNumber = Number(length);
+/**
+ * Turns a number or string into a CSS length value.
+ * Numbers larger than 1 will be interpreted as pixels and converted to the 'rem' unit
+ * Fractions (numbers equal to or smaller than 1, e.g. `1/3`) will be converted to percentages
+ * Strings without a unit (that can be converted to a number) will be treated like numbers
+ * Other strings, e.g. those with a unit ('50%') will be passed through unchanged
+ *
+ * @param {number|string} inputLength - Value to be converted to a CSS length
+ * @returns {string|number} - CSS length
+ */
 
-	if (!Number.isNaN(lengthAsNumber)) {
-		if (lengthAsNumber === 0) {
-			return 0;
+function getLength(inputLength) {
+	const length = Number(inputLength);
+
+	if (!Number.isNaN(length)) {
+		if (length <= 1) {
+			return fractionToPercent(length);
 		}
 
-		return pxToRem(lengthAsNumber);
+		return pxToRem(length);
 	}
 
-	return length;
+	return inputLength;
 }
 
 export default getLength;
