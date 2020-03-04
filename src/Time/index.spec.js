8b8a270359f3a25aa0aa4bfee00b4bffb2e8f7c5
Andrew Dodson <andrew@5app.com>
2020-03-04 12:19:30 +0000

    style(lint): linting

--- src/Time/index.spec.js
+++ src/Time/index.spec.js
@@ -20,25 +20,31 @@ describe('Time', () => {
 			text: '1 minute ago',
 		},
 		{
-			dateTime: "2019-02-27T12:06:14Z",
-			systemTime: "2019-02-29T12:06:14Z",
+			dateTime: '2019-02-27T12:06:14Z',
+			systemTime: '2019-02-29T12:06:14Z',
 			text: 'Wed, 12 PM',
 		},
 	].forEach(({offset, text, dateTime, systemTime}) => {
-		it(`renders time as relative text ${offset ? `, offset:${offset}` : ''}${dateTime ? `, dateTime:${dateTime}` : ''}, expect ${text}`, () => {
-
+		it(`renders time as relative text ${
+			offset ? `, offset:${offset}` : ''
+		}${dateTime ? `, dateTime:${dateTime}` : ''}, expect ${text}`, () => {
 			if (!dateTime) {
 				const date = new Date();
 				date.setTime(date.getTime() - offset * 1000);
 				dateTime = date.toISOString();
 			}
 
-			const {container} = render(<Time dateTime={dateTime} systemTime={systemTime}/>);
+			const {container} = render(
+				<Time dateTime={dateTime} systemTime={systemTime} />
+			);
 
 			const time = container.querySelector('time');
 
 			expect(time).toBeInTheDocument();
-			expect(time).toHaveAttribute('title', (new Date(dateTime)).toLocaleString());
+			expect(time).toHaveAttribute(
+				'title',
+				new Date(dateTime).toLocaleString()
+			);
 			expect(time).toHaveAttribute('dateTime', dateTime);
 			expect(time).toHaveTextContent(text);
 		});
