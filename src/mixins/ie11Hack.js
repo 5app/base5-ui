ba52542c87584499300574591abca1949d21d716
DIonysos Dajka <diondiondion@gmail.com>
2020-03-18 13:01:01 +0100

    refactor: Update IE11 hack to more recent version, noissue

--- src/mixins/ie11Hack.js
+++ src/mixins/ie11Hack.js
@@ -1,6 +1,5 @@
 /* Use this as a media query to apply styles only on IE 11 */
 
-const ie11Hack =
-	'screen and (-ms-high-contrast: active), (-ms-high-contrast: none)';
+const ie11Hack = 'screen and (-ms-high-contrast: none)';
 
 export default ie11Hack;
