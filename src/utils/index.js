388e5a335de77b6e74a761f3e77d213f32393279
DIonysos Dajka <diondiondion@gmail.com>
2020-05-15 15:35:38 +0200

    fix(utils): Fix broken exports from /utils, noissue

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,8 @@
-export {default as colors} from './colors';
+export * from './colors';
 export {default as mergeRefs} from './mergeRefs';
 export {default as getLength} from './getLength';
 export {default as getSpacing} from './getSpacing';
-export {default as spacing} from './spacing';
+export * from './spacing';
 export {default as truncateText} from './truncateText';
-export {default as units} from './units';
+export * from './units';
 export {createStyleFunction} from './styleProps';
