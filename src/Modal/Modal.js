bd20cbe69c91650e5523374622808a23f5fd3dff
DIonysos Dajka <diondiondion@gmail.com>
2020-05-27 14:51:09 +0200

    feat(Modal): Add fullscreen mode, #87

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import FocusLock from 'react-focus-lock';
 
+import Box from '../Box';
 import Portal from '../Portal';
 import CenterContent from '../CenterContent';
 import {useModalManager} from './ModalManager';
@@ -20,6 +21,7 @@ const ModalWrapper = styled.div`
 
 const ContentWrapper = styled.div`
 	position: relative;
+	height: 100%;
 `;
 
 const Overlay = styled.div`
@@ -33,7 +35,48 @@ const Overlay = styled.div`
 	opacity: 0.666;
 `;
 
-function Modal({children, name, onRequestClose, ...otherProps}) {
+function CenterOrFullscreen({
+	breakpoints,
+	children,
+	fullscreen,
+	spacing,
+	width,
+}) {
+	if (fullscreen) {
+		return (
+			<Box
+				height="100%"
+				overflow="hidden"
+				p={spacing}
+				breakpoints={breakpoints}
+			>
+				{children}
+			</Box>
+		);
+	}
+
+	return (
+		<CenterContent
+			fillParent
+			p={spacing}
+			contentWidth={width}
+			breakpoints={breakpoints}
+		>
+			{children}
+		</CenterContent>
+	);
+}
+
+function Modal({
+	breakpoints,
+	children,
+	fullscreen,
+	name,
+	onRequestClose,
+	spacing,
+	width,
+	...otherProps
+}) {
 	const {modalRef, isAtTop} = useModalManager({
 		name,
 		onRequestClose,
@@ -55,10 +98,15 @@ function Modal({children, name, onRequestClose, ...otherProps}) {
 				role="dialog"
 				aria-modal="true"
 			>
-				<CenterContent fillParent>
+				<CenterOrFullscreen
+					fullscreen={fullscreen}
+					spacing={spacing}
+					width={width}
+					breakpoints={breakpoints}
+				>
 					{isAtTop && <Overlay onClick={onRequestClose} />}
 					<FocusLock as={ContentWrapper}>{children}</FocusLock>
-				</CenterContent>
+				</CenterOrFullscreen>
 			</ModalWrapper>
 		</Portal>
 	);
@@ -83,6 +131,36 @@ Modal.propTypes = {
 	 * Add either this or the `aria-label` prop for accessibility.
 	 */
 	'aria-labelledby': PropTypes.string,
+	/**
+	 * When enabled, content will no longer be centred automatically,
+	 * and the modal content can take up the full height of the screen
+	 * when `height: 100%` is applied to it.
+	 *
+	 * Note that in fullscreen mode, the modal content should use
+	 * `overflow: auto` if the content of modal should be scrollable.
+	 */
+	fullscreen: PropTypes.bool,
+	/**
+	 * Defines the minimum spacing between the modal content and
+	 * the edge of the window. Can be an array of responsive values
+	 * when used with the `breakpoints` prop.
+	 */
+	spacing: PropTypes.oneOfType([
+		PropTypes.string,
+		PropTypes.number,
+		PropTypes.array,
+	]),
+	/**
+	 * Set the maximum width of the modal. (It can always go narrower.)
+	 * Can be a responsive array when used with the breakpoints prop.
+	 *
+	 * The width is not applied when the fullscreen prop is active.
+	 */
+	width: PropTypes.oneOfType([
+		PropTypes.number,
+		PropTypes.string,
+		PropTypes.array,
+	]),
 };
 
 // @component
