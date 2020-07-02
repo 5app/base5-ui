76a8e27c046c99b07f2286728e3a30d962834563
DIonysos Dajka <diondiondion@gmail.com>
2020-07-02 16:28:17 +0200

    docs: Fix useReturnToInitEffect docs

--- src/useReturnToInitEffect/index.js
+++ src/useReturnToInitEffect/index.js
@@ -1,13 +1,28 @@
 import {useEffect, useRef} from 'react';
 
+/**
+ * Run an effect only after a condition has returned to its original value.
+ * The callback will not be run initially, only after the value has changed
+ * AND eventually returned to its original value.
+ *
+ * @param {Function} callback - Function to run after the status parameter
+ * has changed and then returns to its initial value
+ * @param {bool|number|string} status - Status that should be tracked to
+ * decide whether to run the effect
+ */
+
 function useReturnToInitEffect(callback, state) {
 	const initialState = useRef(state);
 	const shouldRunEffect = useRef(false);
 	const callbackRef = useRef(callback);
 
 	useEffect(() => {
-		// We're storing the callback in a ref since
-		// we don't want our
+		// We're storing the latest callback in a ref since
+		// we don't want our main effect (below) to re-run
+		// when the callback changes (which it would do
+		// on every render when defined as an inline function).
+		// But we still want it to have access to the latest
+		// callback! This is a way of achieving that.
 		callbackRef.current = callback;
 	}, [callback]);
 
