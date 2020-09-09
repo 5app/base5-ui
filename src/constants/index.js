3c29e303f3f9b8e23143682201bb41dca52982e6
DIonysos Dajka <diondiondion@gmail.com>
2020-09-09 10:50:04 +0200

    fix(constants): Add more key codes, noissue

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,11 +4,23 @@
  * These ones have to be matched against `event.keyCode`.
  */
 const KEY_CODES = {
-	ESC: 27,
+	BACKSPACE: 8,
+	TAB: 9,
 	ENTER: 13,
+	SHIFT: 16,
+	CONTROL: 17,
+	ESC: 27,
 	SPACE: 32,
+	PAGE_UP: 33,
+	PAGE_DOWN: 34,
+	END: 35,
+	HOME: 36,
+	ARROW_LEFT: 37,
 	ARROW_UP: 38,
+	ARROW_RIGHT: 39,
 	ARROW_DOWN: 40,
+	DELETE: 46,
+	META: 91,
 };
 
 export {KEY_CODES};
