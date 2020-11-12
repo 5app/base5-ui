bf3acac3207bcf1ca8ca32a55d8b5a693588e67a
DIonysos Dajka <diondiondion@gmail.com>
2020-11-12 16:30:26 +0100

    feat(useOnClickOutside): Allow passing multiple refs

--- src/useOnClickOutside/index.js
+++ src/useOnClickOutside/index.js
@@ -10,22 +10,27 @@ import useEventListener from '../useEventListener';
 
 export function getOutsideTargetCallback(excludedElementRef, callback) {
 	return event => {
-		const excludedElement =
-			excludedElementRef && excludedElementRef.current;
+		const excludedRefs = Array.isArray(excludedElementRef)
+			? excludedElementRef
+			: [excludedElementRef];
 
-		// Bail out if the clicked element or the currently focused element
-		// is inside of excludedElement. We need to check the focused element
-		// to prevent an issue in Chrome where initiating a drag inside of an
-		// input (to select the text inside of it) and ending that drag outside
-		// of the input fires a click event, breaking our excludedElement rule.
-		if (
-			excludedElement &&
-			(excludedElement === event.target ||
-				excludedElement.contains(event.target) ||
-				excludedElement === document.activeElement ||
-				excludedElement.contains(document.activeElement))
-		) {
-			return null;
+		for (const ref of excludedRefs) {
+			const excludedElement = ref && ref.current;
+
+			// Bail out if the clicked element or the currently focused element
+			// is inside of excludedElement. We need to check the focused element
+			// to prevent an issue in Chrome where initiating a drag inside of an
+			// input (to select the text inside of it) and ending that drag outside
+			// of the input fires a click event, breaking our excludedElement rule.
+			if (
+				excludedElement &&
+				(excludedElement === event.target ||
+					excludedElement.contains(event.target) ||
+					excludedElement === document.activeElement ||
+					excludedElement.contains(document.activeElement))
+			) {
+				return null;
+			}
 		}
 
 		callback(event);
