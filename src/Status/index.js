340cdad16801b366c02309c5c54f2b5ac9f5fcff
Dionysos Dajka <diondiondion@gmail.com>
2021-01-04 15:46:11 +0100

    refactor(Status): Better defaults for role="alert"

--- src/Status/index.js
+++ src/Status/index.js
@@ -4,17 +4,23 @@ import PropTypes from 'prop-types';
 import Box from '../Box';
 
 const Status = forwardRef((props, ref) => {
+	const {role, atomic, 'aria-live': ariaLive, ...otherProps} = props;
+	const ariaLiveDefault = role === 'alert' ? 'assertive' : 'polite';
+
 	return (
 		<Box
 			ref={ref}
-			role="status"
-			aria-live="polite"
-			aria-relevant="additions text"
-			{...props}
-			aria-atomic={props.atomic ? 'true' : props['aria-atomic']}
+			{...otherProps}
+			role={role}
+			aria-live={ariaLive || ariaLiveDefault}
+			aria-atomic={atomic ? 'true' : props['aria-atomic']}
 		/>
 	);
 });
+
+Status.defaultProps = {
+	role: 'status',
+};
 
 Status.displayName = 'Status';
 
