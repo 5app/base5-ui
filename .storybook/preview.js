227e5eb743b0af1b50e8948c0cadd438ef11d7a7
Dionysos Dajka <diondiondion@gmail.com>
2021-06-08 18:17:17 +0200

    chore: Replace Docz with Storybook, #135

---
+++ .storybook/preview.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import 'focus-visible';
+
+import ThemeSection from '../src/ThemeSection';
+import theme from '../src/theme';
+
+import GlobalStyles from './GlobalStyles';
+
+export const decorators = [
+  (Story) => (
+    <ThemeSection name="page" baseTheme={theme}>
+      <GlobalStyles />
+      {Story()}
+    </ThemeSection>
+  ),
+];
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  options: {
+    storySort: {
+      order: ['base5-ui', 'Components'],
+    },
+  },
+  backgrounds: {
+    disable: true,
+    grid: {
+      disable: true
+    }
+  }
+}
