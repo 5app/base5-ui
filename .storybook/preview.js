1b2008d142dabadfc40e5c60a4f15eec8f788664
Dionysos Dajka <diondiondion@gmail.com>
2021-06-18 10:51:17 +0200

    docs: Fix MDX Canvas component missing theme, noissue

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Canvas} from '@storybook/addon-docs/blocks';
 
 import 'focus-visible';
 
@@ -8,31 +9,40 @@ import theme from '../src/theme';
 import GlobalStyles from './GlobalStyles';
 
 export const decorators = [
-  (Story) => (
-    <ThemeSection name="page" baseTheme={theme}>
-      <GlobalStyles />
-      {Story()}
-    </ThemeSection>
-  ),
+	Story => (
+		<ThemeSection name="page" baseTheme={theme}>
+			<GlobalStyles />
+			{Story()}
+		</ThemeSection>
+	),
 ];
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-  options: {
-    storySort: {
-      order: ['base5-ui', 'Components'],
-    },
-  },
-  backgrounds: {
-    disable: true,
-    grid: {
-      disable: true
-    }
-  }
-}
+	actions: {argTypesRegex: '^on[A-Z].*'},
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
+	},
+	options: {
+		storySort: {
+			order: ['base5-ui', 'Components'],
+		},
+	},
+	backgrounds: {
+		disable: true,
+		grid: {
+			disable: true,
+		},
+	},
+	docs: {
+		components: {
+			Canvas: ({children, ...props}) => (
+				<ThemeSection name="page" baseTheme={theme}>
+					<Canvas {...props}>{children}</Canvas>
+				</ThemeSection>
+			),
+		},
+	},
+};
