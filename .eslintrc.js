f34cd25b83c0e1e2569e6de796a33a0f5a7d0584
Gabriel Hernandez <ghernandez345@gmail.com>
2021-08-04 12:51:44 +0100

    chore: udate eslint to use relevant overrides for TS files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,12 @@
 module.exports = {
-	plugins: ['react-hooks', 'jsx-a11y', '@typescript-eslint'],
+	plugins: ['react-hooks', 'jsx-a11y'],
 	extends: [
 		'5app',
 		'plugin:react/recommended',
 		'plugin:jsx-a11y/recommended',
 		'prettier',
-		'plugin:@typescript-eslint/eslint-recommended',
-		'plugin:@typescript-eslint/recommended'
 	],
-	parser: '@typescript-eslint/parser',
+	parser: 'babel-eslint',
 	parserOptions: {
 		allowImportExportEverywhere: true,
 		ecmaFeatures: {
@@ -22,22 +20,58 @@ module.exports = {
 		jest: true,
 	},
 	rules: {
+		'no-unused-vars': [2, {ignoreRestSiblings: true}],
 		'no-console': [2, {allow: ['error', 'warn']}],
 		'prefer-template': 0,
 		'react/jsx-no-bind': [2, {allowArrowFunctions: true, ignoreRefs: true}],
 		'react/prop-types': 0,
 		'react-hooks/rules-of-hooks': 'error',
 		'react-hooks/exhaustive-deps': 'warn',
-
-		// disabling base rule and enabling TS rule so there is not false error.
-		'no-use-before-define': 'off',
-		'@typescript-eslint/no-use-before-define': 'error',
-		'no-unused-vars': 'off',
-		'@typescript-eslint/no-unused-vars': [2, {ignoreRestSiblings: true}]
 	},
-	settings: {
-		react: {
-			version: 'detect',
-		},
+	overrides: [
+		{
+			files: ['**/*.ts', '**/*.tsx'],
+			plugins: ['react-hooks', 'jsx-a11y', '@typescript-eslint'],
+			extends: [
+				'5app',
+				'plugin:react/recommended',
+				'plugin:jsx-a11y/recommended',
+				'prettier',
+				'plugin:@typescript-eslint/eslint-recommended',
+				'plugin:@typescript-eslint/recommended'
+			],
+			parser: '@typescript-eslint/parser',
+			parserOptions: {
+				allowImportExportEverywhere: true,
+				ecmaFeatures: {
+					experimentalObjectRestSpread: true,
+					jsx: true,
+					},
+				sourceType: 'module',
+			},
+			env: {
+				browser: true,
+				jest: true,
+			},
+			rules: {
+				'no-console': [2, {allow: ['error', 'warn']}],
+				'prefer-template': 0,
+				'react/jsx-no-bind': [2, {allowArrowFunctions: true, ignoreRefs: true}],
+				'react/prop-types': 0,
+				'react-hooks/rules-of-hooks': 'error',
+				'react-hooks/exhaustive-deps': 'warn',
+
+				// you must disable the base rule as it can report incorrect errors. more info here:
+				// https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-use-before-define.md
+				'no-use-before-define': 'off',
+				'@typescript-eslint/no-use-before-define': 'error',
+
+				'no-unused-vars': 'off',
+				'@typescript-eslint/no-unused-vars': [2, {ignoreRestSiblings: true}]
+			},
+			settings: { react: { version: 'detect' } }
+		}
+	],
+	settings: { react: { version: 'detect'},
 	}
 };
